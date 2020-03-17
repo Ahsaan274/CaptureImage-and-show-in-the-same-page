75fb9e1b3d54226ffac3b7326334b5e9588d02b8
Ahsaan Ansari <ahsaan.se15@gmail.com>
2020-03-17 10:31:47 +0500

    Add project files.

---
+++ CaptureImage/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CaptureImage
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+            {
+                await DisplayAlert("Ops", "Unable to capture an image!", "OK");
+
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Demo"
+                });
+
+            if (file == null)
+                return;
+
+            srcc = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+
+            });
+            MyImage.Source = srcc;
+         
+        }
+        ImageSource srcc;
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            WelcomePage wp = new WelcomePage();
+            wp.src = srcc;
+            Application.Current.MainPage = new NavigationPage(wp);
+        }
+    }
+}
---
+++ CaptureImage/WelcomePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CaptureImage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public ImageSource src;
+        public WelcomePage()
+        {
+            InitializeComponent();
+            
+          
+            
+            
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //your code here;
+
+            img.Source = "a.png";
+            img.Source = src;
+        }
+
+    }
+}
